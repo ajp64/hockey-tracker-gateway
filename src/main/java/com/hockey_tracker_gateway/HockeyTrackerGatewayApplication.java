4544d08e8a024ca526ff805826f6c72a9ac3fff1
ajp64 <antonyjparnell@gmail.com>
2025-03-05 21:57:09 +0000

    Initial set up files

---
+++ src/main/java/com/hockey_tracker_gateway/HockeyTrackerGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hockey_tracker.hockey_tracker_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HockeyTrackerGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HockeyTrackerGatewayApplication.class, args);
+	}
+
+}
